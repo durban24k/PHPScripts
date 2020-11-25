216dc6fe3c61427366dfc6e79a3a8849b3280f00
Wanjau Alfred Mambo <56720300+durban24k@users.noreply.github.com>
2020-11-25 20:04:44 +0300

    for loop

---
+++ ussd_function.php
@@ -0,0 +1,13 @@
+<?php
+include_once 'keys.php';
+include_once 'functions.php';
+
+$input='1*Alfred*Wanjau*0712345678*1*';
+
+$explode_string=explode('*',$input);
+$len=count($explode_string);
+for($x=0;$x<$len;$x++){
+     echo "$explode_string[$x] <br>";
+}
+echo $key;
+?>
